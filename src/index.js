010c3bb452dbe53bbce5ffa13394ec362540241e
kubzer0 <jakub.gryka@gmail.com>
2018-12-10 12:24:01 +0100

    more tests

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
-export const orderTotal = (arrayofOrders) => (
-    arrayofOrders.reduce(
-        (reduced, item) =>
-            reduced + (item.price * (item.quantity === undefined? 1 :  item.quantity )),
-        0)
-)
+export const orderTotal = (arrayOfOrders) => {
+    const preTotal = arrayOfOrders.reduce(
+      (reduced, item) => (
+        item.quantity !== undefined ?
+          reduced + (item.price * item.quantity)
+          :
+          reduced + item.price
+      ),
+      0
+    )
+  
+    const shipping = arrayOfOrders.find(item => item.shipping)
+  
+    return (
+      shipping ?
+        preTotal >= (shipping.freeShipping + shipping.price) ?
+          preTotal - shipping.price
+          :
+          preTotal
+        :
+        preTotal
+    )
+  }
